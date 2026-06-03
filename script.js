5e3e3b8e6b4e628ce91238bc5665ea85409fb1ec
Muhammad Usama <143987957+Sammalik8884@users.noreply.github.com>
2026-06-03 14:23:02 +0500

    Refactor button click event handler in script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const button = document.getElementById('color-btn');
+const button = document.getElementById('color-btn')
 
-button.addEventListener("click", () => {
-    const randomColor =
-        "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
+button.addEventListener('click', () => {
+  const randomColor =
+    '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')
 
-    document.body.style.backgroundColor = randomColor;
-});
+  document.body.style.backgroundColor = randomColor
+})
